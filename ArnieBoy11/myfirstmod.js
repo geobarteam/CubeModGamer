c0c3e8000b2a90bd282a839b38e6d9f812441b85
Arnieboy545 <arno.vandiest@hotmail.com>
2016-02-27 13:42:53 +0100

    Update myfirstmod.js

--- ArnieBoy11/myfirstmod.js
+++ ArnieBoy11/myfirstmod.js
@@ -1,1 +1,15 @@
 
+exports.myfirstmod=function(){
+  var d = box( blocks.oak )
+          .up()
+          .box( blocks.oak ,3,1,3)
+          .down()
+          .fwd(2)
+          .box( blocks.oak )
+          .turn()
+          .fwd(2)
+          .box( blocks.oak )
+          .turn()
+          .fwd(2)
+          .box( blocks.oak );
+}
